96f833c500f21b9d50f520cb007e758f3477b648
Amr Gamal <ag.amrgamal91@gmail.com>
2020-11-09 01:41:21 +0200

    initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Notification from "./components/Notification";
+import Popup from "./components/Popup";
+
+import { useState, useEffect } from "react";
+import {showNotification as show} from './helpers/helpers'
+const words = [
+  "development",
+  "movies",
+  "advertisement",
+  "weather",
+  "theater",
+  "football",
+  "example"
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setplayable] = useState(true);
+  const [correctLetters, setcorrectLetters] = useState([]);
+  const [wrongLetters, setworngLetters] = useState([]);
+  const [showNotification, setshowNotification] = useState(false) 
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setcorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setshowNotification)
+          }
+        } else {
+          //not exist in selected word
+          if (!wrongLetters.includes(letter)) {
+            setworngLetters(wrongLetters => [...wrongLetters, letter]);
+          } else {
+            show(setshowNotification)
+          }
+        }
+      }
+    };
+    window.addEventListener('keydown',handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown',handleKeyDown);
+    };
+  }, [correctLetters,wrongLetters,playable]);
+
+  function playAgain(){
+    setplayable(true);
+
+    //Empty Arrays
+    setcorrectLetters([])
+    setworngLetters([])
+    
+    const random=Math.floor(Math.random()*words.length)
+    selectedWord=words[random]
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters}/>
+        <WrongLetters wrongLetters={wrongLetters}/>
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord}  
+      setPlayable={setplayable} playAgain={playAgain}/>
+      <Notification showNotification={showNotification}/>
+    </>
   );
 }
 
